a3722f508837eae155ff2e4cb4da765b311589f8
SpahlMC <mark.spahl@ucalgary.ca>
2026-07-24 00:03:10 -0400

    Updated genome_scan for multi sequence

--- genome_scan.py
+++ genome_scan.py
@@ -19,9 +19,46 @@ from keras.models import load_model
 from tqdm import tqdm
 import shutil
 import argparse
+import tempfile
+from pathlib import Path
 
+WINDOW_SIZE = 101  # window size for sliding windows, fixed to 101
+OUTPUT_COLS = ['chrom', 'start', 'end', 'name', 'score', 'strand']
+SAMPLE_FASTA = "df_sample.fasta"
+OUTPUT_SAMPLE = 'output_sample'
+ILEARNPLUS_FILE = "iLearnPlus/util/FileProcessing.py"
 
-def extract_sliding_windows(ref_genome_file: str, window_size: int,
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f"  , "--fasta"     , help="FASTA sequences file.", type=str)
+    parser.add_argument("-o"  , "--output-dir", help='Output folder.', type=str)
+    parser.add_argument("-r"  , "--repo-dir"  , help='Repo folder.', type=str)
+    parser.add_argument("-ss", "--step-size"  , help='Sliding window step size. Default is 3.', type=int, default=3)
+    parser.add_argument("-bs", "--batch-size" , help='Batch size for iLearnPlus feature generation.', type=int, default=10000)
+    parser.add_argument("-ms", "--min-score" , help='Minimum score for retention.', type=float, default=0.3)
+    
+    return parser.parse_args()
+
+def extract_windows_from_sequence(seq_id, seq: Seq, window_size: int, step_size: int):
+    seq_len = len(seq)
+    windows = []
+    u_id = 0
+    for i in tqdm(range(0, seq_len - window_size + 1, step_size)):
+        u_id += 1
+        window_start = i
+        window_end = i + window_size
+        window_seq = str(seq[window_start:window_end])
+        windows.append(
+            (u_id, seq_id, window_start, window_end, window_seq, '+'))
+        revcomp = str(Seq(window_seq).reverse_complement())
+        windows.append(
+            (u_id, seq_id, window_start, window_end, revcomp, '-'))
+
+    df = pd.DataFrame(windows, columns=['u_id', 'seq_id', 'start', 'end', 'seq', 'strand'])
+    
+    return df
+
+def extract_sliding_windows(ref_genome_dict: dict, window_size: int,
                             step_size: int) -> pd.DataFrame:
     """This function extracts sliding windows from a reference genome.
 
@@ -33,12 +70,10 @@ def extract_sliding_windows(ref_genome_file: str, window_size: int,
     Returns:
         pd.DataFrame: this is a Pandas DataFrame containing the sliding windows
     """
-    # Load reference genome into memory
-    ref_genome = SeqIO.to_dict(SeqIO.parse(ref_genome_file, "fasta"))
 
     # Define function to extract windows from a single sequence
     def extract_windows_from_sequence(seq_id):
-        seq = ref_genome[seq_id].seq
+        seq = ref_genome_dict[seq_id].seq
         seq_len = len(seq)
         windows = []
         u_id = 0
@@ -55,7 +90,7 @@ def extract_sliding_windows(ref_genome_file: str, window_size: int,
         return windows
 
     windows = []
-    for seq_id in ref_genome.keys():
+    for seq_id in ref_genome_dict.keys():
         seq_windows = extract_windows_from_sequence(seq_id)
         windows.extend(seq_windows)
 
@@ -181,10 +216,12 @@ def read_csv_low(file, data_path, input_dim):
 
     return x, sample_names
 
-def create_result_df(df, output_cols, min_score):
+def create_result_df_old(df, output_cols, min_score):
     # Remove _ for string splitting
     df['SampleName'] = df['SampleName'].str.replace('NC_0', 'NC0')
     # String splitting
+    print("Processing result df head")
+    print(df.head)
     split_names = df['SampleName'].str.split('_', expand=True)
     # len(split_names) with no _ in 'chrom" is 5. 
     # 'chrom' is contained in indices 1:len(split_names)-4
@@ -208,21 +245,37 @@ def create_result_df(df, output_cols, min_score):
 
     return df_result
 
-if __name__ == '__main__':
+def create_result_df(df, output_cols, min_score):
+    # Remove _ for string splitting
+    df['SampleName'] = df['SampleName'].str.replace('NC_0', 'NC0')
+    # String splitting
+    print("Processing result df head")
+    print(df.head)
+
+    # Create start and end indices
+    df['start'] = df['start'].astype(int)
+    df['end'] = df['end'].astype(int)
+    # Define Strand and sample name
+    df['chrom'] = df['seq_id']
+    # Add metadata
+    df['chrom'] = df['chrom'].str.replace('NC0', 'NC_0')
+    df['name'] = 'bactermfinder'
+    df['score'] = df['probability_mean']
+    # Sorting columns for a bedfile
+    df_result = df[output_cols]
+    # Filtering based on the threhsold
+    df_result = df_result[df_result['score']>min_score]
+
+    return df_result
+
+def main():
     # time it
     start_time = time.time()
 
     ############################################ Sys inputs ########################################################
     # get the parameters from the command line
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f"  , "--fasta"     , help="FASTA sequences file.", type=str)
-    parser.add_argument("-o"  , "--output-dir", help='Output folder.', type=str)
-    parser.add_argument("-r"  , "--repo-dir"  , help='Repo folder.', type=str)
-    parser.add_argument("-ss", "--step-size"  , help='Sliding window step size. Default is 3.', type=int, default=3)
-    parser.add_argument("-bs", "--batch-size" , help='Batch size for iLearnPlus feature generation.', type=int, default=10000)
-    parser.add_argument("-ms", "--min-score" , help='Minimum score for retention.', type=float, default=0.3)
-    
-    args = parser.parse_args()
+    args = parse_args()
+
     genome_file = args.fasta
     output_dir = args.output_dir
     repo_dir = args.repo_dir
@@ -230,153 +283,174 @@ if __name__ == '__main__':
     batch_size = args.batch_size
     min_score = args.min_score
 
-    #genome_file = sys.argv[1]  # genome file name fasta format
-    #step_size = int(sys.argv[2])  # step size for sliding window ( aka stride size )
-    #output_dir = sys.argv[3]  # output file name
-    #batch_size = int(sys.argv[4])  # batch size for iLearnPlus feature generation
-    WINDOW_SIZE = 101  # window size for sliding windows, fixed to 101
-    OUTPUT_COLS = ['chrom', 'start', 'end', 'name', 'score', 'strand']
-    ############################################ Paths & Filenames ########################################################
-    SAMPLE_FASTA = "df_sample.fasta"
-    OUTPUT_SAMPLE = 'output_sample'
-    ilearnplus_file = "iLearnPlus/util/FileProcessing.py"
+    ############################################ Filenames and paths ########################################################
+
     genome_filename = os.path.basename(genome_file)
-    ilearnplus_path = os.path.join(repo_dir, ilearnplus_file)
+    ilearnplus_path = os.path.join(repo_dir, ILEARNPLUS_FILE)
     ############################################ sliding window generation ##########################################
-    # get the sequences
-    print("\n")
-    print("Sliding windows generation started\n")
     
-    if os.path.exists('Sample.csv'):
-        os.remove('Sample.csv')
-    df_slide = extract_sliding_windows(genome_file, WINDOW_SIZE, step_size)
-    sliding_windows_filename = f'_{genome_filename}_sliding_windows.csv'
-    df_slide.to_csv(os.path.join(output_dir, sliding_windows_filename),
-                    index=False)
-    print("\nSliding windows generated")
-    ############################################  iLearnPlus ########################################################
-    if os.path.exists('df_sample.fasta'):
-        os.remove('df_sample.fasta')
-    print("\n")
-    print("iLearnPlus biological feature generation started")
-    # convert the dataframe to fasta format
-    sample_fasta_path = os.path.join(output_dir, SAMPLE_FASTA)
-    df_to_fasta(df_slide.reset_index(drop=True), sample_fasta_path, "training")
+    # Load reference genome into memory
+    ref_genome_dict = SeqIO.to_dict(SeqIO.parse(genome_file, "fasta"))
+    write_df = pd.DataFrame(columns=OUTPUT_COLS)
+    for seq_id in genome_filename:
 
-    # remove output folder if it exists
-    output_sample_dir = os.path.join(output_dir, OUTPUT_SAMPLE)
-    if os.path.exists(output_sample_dir):
-        shutil.rmtree(output_sample_dir)
+        seq = ref_genome_dict[seq_id].seq
+        df_slide = extract_windows_from_sequence(seq_id, seq, WINDOW_SIZE, step_size)
 
-    # if os.path.exists('output_sample_merged'):
-    #     os.remove('output_sample_merged')
-
-    # generate features
-    os.system(f'python {ilearnplus_path} ' +
-              sample_fasta_path + ' ' + str(batch_size) + ' ' + '16' + ' ' +
-              output_sample_dir)
-
-    files = os.listdir(output_sample_dir)
-    
-    number_of_batches = len(df_slide) // batch_size
-    print("number_of_batches", number_of_batches )
-    print("\niLearnPlus biological feature generation finished")
-    endtime = time.time()
-    print("Feature generation took", endtime - start_time, "s.")
-
-    ############################################ Loading and prediction ########################################################
-
-    # check gpu exists
-    if os.system('nvidia-smi') == 0:
-        gpu_exists = 1
-        print('###### GPU exists, predicting with GPU ########')
-    else:
-        gpu_exists = 0
-        print('###### GPU doesn\'t exists, predicting with CPU  ########')
-
-    input_dim_dict = {
-        'ENAC.csv': (97, 4),
-        'PS2.csv': (100, 16),
-        'NCP.csv': (101, 3),
-        'binary.csv': (101, 4),
-    }
-    
-
-    data_path = os.path.join(output_dir, 'output_sample/')
-    print('\nLoading data')
-    # files = os.listdir(data_path)
-    for embedding in input_dim_dict.keys():
-        # load deep learning model
-        print(f"\nLoading Deep Learning Model {embedding} \n")
-        model_filename = f'deep-bactermfinder_3cnn_1d_1cat_reduced_10x_{embedding}_saved_model.h5'
-        model = load_model(os.path.join(repo_dir, model_filename))
-
-        embedding_wo_csv = embedding.split('.csv')[0]
-        out_embed = pd.DataFrame(columns=['SampleName', f'probability_{embedding_wo_csv}'])
+    # Convert windows to a Pandas DataFrame
         
-        for batch in range(number_of_batches + 1):
-            dp = f'{data_path}{embedding_wo_csv}-{batch}.csv'
-            x, sample_info = read_csv_low(embedding, dp, input_dim_dict[embedding])
-            print(f'x shape of {embedding} is: {x.shape}')
 
-            print("Predicting sequences")
-            with tf.device('/gpu:0'):
-                y_pred = model.predict(x)
+        with tempfile.TemporaryDirectory() as temp_dir_str:
 
-            # appending the results
-            print("Appending the results")
+            temp_dir = Path(temp_dir_str)
 
-            sample_info = sample_info.reset_index()
-            y_pred = pd.DataFrame(y_pred, )
-            y_pred[f'probability_{embedding_wo_csv}'] = y_pred[0]
+            # get the sequences
+            print("\n")
+            print("Sliding windows generation started\n")
             
-            y_pred = pd.concat([sample_info, y_pred], axis=1)
+            if os.path.exists('Sample.csv'):
+                os.remove('Sample.csv')
+            #df_slide = extract_sliding_windows(genome_file, WINDOW_SIZE, step_size)
+            sliding_windows_filename = f'_{genome_filename}_sliding_windows.csv'
+            df_slide.to_csv(os.path.join(temp_dir, sliding_windows_filename),
+                            index=False)
+            print("\nSliding windows generated")
+            ############################################  iLearnPlus ########################################################
+            if os.path.exists('df_sample.fasta'):
+                os.remove('df_sample.fasta')
+            print("\n")
+            print("iLearnPlus biological feature generation started")
+            # convert the dataframe to fasta format
+            sample_fasta_path = os.path.join(temp_dir, SAMPLE_FASTA)
+            df_to_fasta(df_slide.reset_index(drop=True), sample_fasta_path, "training")
 
-            out_embed = pd.concat([out_embed, y_pred],ignore_index=True)
-            del x
-            del y_pred
-            del sample_info
+            # remove output folder if it exists
+            output_sample_dir = os.path.join(temp_dir, OUTPUT_SAMPLE)
+            if os.path.exists(output_sample_dir):
+                shutil.rmtree(output_sample_dir)
+
+            # if os.path.exists('output_sample_merged'):
+            #     os.remove('output_sample_merged')
+
+            # generate features
+            os.system(f'python {ilearnplus_path} ' +
+                    sample_fasta_path + ' ' + str(batch_size) + ' ' + '16' + ' ' +
+                    output_sample_dir)
+
+            files = os.listdir(output_sample_dir)
             
-                 
-        # write the results
-        out_embed.drop(columns=['index', 0], inplace=True)
-        embed_filename = '_' + str(embedding_wo_csv) + genome_filename + '.csv'
-        out_embed.to_csv(os.path.join(output_dir, embed_filename), index=False)
+            number_of_batches = len(df_slide) // batch_size
+            print("number_of_batches", number_of_batches )
+            print("\niLearnPlus biological feature generation finished")
+            endtime = time.time()
+            print("Feature generation took", endtime - start_time, "s.")
 
-        del out_embed
-        del model
+            ############################################ Loading and prediction ########################################################
 
-    endtime_pred = time.time()
-    print("Predicting sequences took", endtime_pred - endtime, "s.")
-    
-    # JOIN THE RESULTS, 
-    # all of the columns in the rsults are the same exp the probability column
-    # so we need to just add that column to the first dataframe
-    
-    # read the first file
-    embed_filename = '_' + str(embedding_wo_csv) + genome_filename + '.csv'
-    df = pd.read_csv(os.path.join(output_dir, embed_filename))
-    # read the rest of the files
-    for embedding in input_dim_dict.keys():
-        if embedding != 'binary.csv':
-            embedding_wo_csv = embedding.split('.csv')[0]
-            embed_filename = '_' + embedding_wo_csv + genome_filename +  '.csv'
-            df1 = pd.read_csv(os.path.join(output_dir, embed_filename))
-            df[f'probability_{embedding_wo_csv}'] = df1[f'probability_{embedding_wo_csv}']
-            del df1
-    # write the results
-    df['probability_mean'] = df[ [col for col in df.columns if 'probability' in col] ].mean(axis=1)
-    mean_filename = genome_filename + '_mean.csv'
-    df.to_csv(os.path.join(output_dir, mean_filename), index=False)
-    result_df = create_result_df(df, OUTPUT_COLS, min_score)
+            # check gpu exists
+            if os.system('nvidia-smi') == 0:
+                gpu_exists = 1
+                print('###### GPU exists, predicting with GPU ########')
+            else:
+                gpu_exists = 0
+                print('###### GPU doesn\'t exists, predicting with CPU  ########')
+
+            input_dim_dict = {
+                'ENAC.csv': (97, 4),
+                'PS2.csv': (100, 16),
+                'NCP.csv': (101, 3),
+                'binary.csv': (101, 4),
+            }
+            
+
+            data_path = os.path.join(temp_dir, 'output_sample/')
+            print('\nLoading data')
+            # files = os.listdir(data_path)
+            for embedding in input_dim_dict.keys():
+                # load deep learning model
+                print(f"\nLoading Deep Learning Model {embedding} \n")
+                model_filename = f'deep-bactermfinder_3cnn_1d_1cat_reduced_10x_{embedding}_saved_model.h5'
+                model = load_model(os.path.join(repo_dir, model_filename))
+
+                embedding_wo_csv = embedding.split('.csv')[0]
+                out_embed = pd.DataFrame(columns=['SampleName', f'probability_{embedding_wo_csv}'])
+                
+                for batch in range(number_of_batches + 1):
+                    dp = f'{data_path}{embedding_wo_csv}-{batch}.csv'
+                    x, sample_info = read_csv_low(embedding, dp, input_dim_dict[embedding])
+                    print(f'x shape of {embedding} is: {x.shape}')
+
+                    print("Predicting sequences")
+                    with tf.device('/gpu:0'):
+                        y_pred = model.predict(x)
+
+                    # appending the results
+                    print("Appending the results")
+
+                    sample_info = sample_info.reset_index()
+                    y_pred = pd.DataFrame(y_pred, )
+                    y_pred[f'probability_{embedding_wo_csv}'] = y_pred[0]
+                    
+                    y_pred = pd.concat([sample_info, y_pred], axis=1)
+
+                    out_embed = pd.concat([out_embed, y_pred],ignore_index=True)
+                    del x
+                    del y_pred
+                    del sample_info
+                    
+                        
+                # write the results
+                out_embed.drop(columns=['index', 0], inplace=True)
+                embed_filename = '_' + str(embedding_wo_csv) + genome_filename + '.csv'
+                out_embed.to_csv(os.path.join(temp_dir, embed_filename), index=False)
+
+                del out_embed
+                del model
+
+            endtime_pred = time.time()
+            print("Predicting sequences took", endtime_pred - endtime, "s.")
+            
+            # JOIN THE RESULTS, 
+            # all of the columns in the rsults are the same exp the probability column
+            # so we need to just add that column to the first dataframe
+            
+            # read the first file
+            #embed_filename = '_' + str(embedding_wo_csv) + genome_filename + '.csv'
+            #df = pd.read_csv(os.path.join(output_dir, embed_filename))
+            # read the rest of the files
+            #for embedding in input_dim_dict.keys():
+            #    if embedding != 'binary.csv':
+            #        embedding_wo_csv = embedding.split('.csv')[0]
+            #        embed_filename = '_' + embedding_wo_csv + genome_filename +  '.csv'
+            #        df1 = pd.read_csv(os.path.join(output_dir, embed_filename))
+            #        df[f'probability_{embedding_wo_csv}'] = df1[f'probability_{embedding_wo_csv}']
+            #        del df1
+            
+            # Append embeddings to df_slide
+            for embedding in input_dim_dict.keys():
+                embedding_wo_csv = embedding.split('.csv')[0]
+                embed_filename = '_' + embedding_wo_csv + genome_filename +  '.csv'
+                df1 = pd.read_csv(os.path.join(output_dir, embed_filename))
+                df_slide[f'probability_{embedding_wo_csv}'] = df1[f'probability_{embedding_wo_csv}']
+                del df1
+            # write the results
+            df_slide['probability_mean'] = df_slide[ [col for col in df_slide.columns if 'probability' in col] ].mean(axis=1)
+            mean_filename = genome_filename + '_mean.csv'
+            df_slide.to_csv(os.path.join(output_dir, mean_filename), index=False)
+            result_df = create_result_df(df_slide, OUTPUT_COLS, min_score)
+            pd.concat([write_df, result_df], columns=OUTPUT_COLS)
+
     result_filename = genome_filename + '_result.csv'
     print("RESULT FILENAME GENOME SCAN")
     print(result_filename)
     print(str(os.path.join(output_dir, result_filename)))
     result_df.to_csv(os.path.join(output_dir, result_filename), index=False)
-    
+
     ############################################ timing and done ########################################################
     endtime = time.time()
     print("Totally, it took", endtime - start_time,
           f"s to find the terminators of a genome file that has {len(df_slide)} windows")
     print('Done')
+
+if __name__ == '__main__':
+    main()
